afa1dd96209c4f399ec2e07ae6b40587d179c30f
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 01:28:03 +0200

    initial state

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'witchparty'`, () => {
+  it(`should have as title 'babysitting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('witchparty');
+    expect(app.title).toEqual('babysitting');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('witchparty app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('babysitting app is running!');
   });
 });
