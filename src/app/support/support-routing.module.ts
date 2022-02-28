7acbec7cc5ca60d695a8bb3caca5942df74158f0
pankowskitomasz <pankowski.email@gmail.com>
2022-02-28 21:32:14 +0200

    routing

--- src/app/support/support-routing.module.ts
+++ src/app/support/support-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { SupportComponent } from './support/support.component';
 
 const routes: Routes = [
   {
-    path:"comingsoon",
-    component: ComingsoonComponent
+    path: "support",
+    component: SupportComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ComingsoonRoutingModule { }
+export class SupportRoutingModule { }
---
+++ src/app/support/support/support.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  templateUrl: './support.component.html',
+  styleUrls: [
+    './support.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class SupportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
